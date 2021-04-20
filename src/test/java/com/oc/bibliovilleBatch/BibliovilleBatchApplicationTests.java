b4b89a78d7af6d3a4c76e41a6f9df1cc1952462d
MokoMaracucho <desmoulin.federico@gmail.com>
2021-04-20 11:01:01 +0200

    Spring Initializr

---
+++ src/test/java/com/oc/bibliovilleBatch/BibliovilleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.bibliovilleBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliovilleBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
